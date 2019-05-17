ad22eaec3fc030622cc8a41a7e62a463c7159a64
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-05-17 16:22:49 +0200

    functional wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'faker'
+
+10.times do
+  new_article = Article.new(title: Faker::Artist.name, content: Faker::Quotes::Shakespeare.hamlet_quote)
+  new_article.save
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +12,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
